fac4f752c35bf9a400d86de402c853b81f42f36d
aotkub <nattapat.aot7735@gmail.com>
2025-05-19 16:49:42 +0700

    แก้ไข

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const fields = [
     { selectId: "small", otherId: "small-other", hiddenId: "small-hidden" },
     { selectId: "durio-Zibethinus", otherId: "durio-Zibethinus-other", hiddenId: "durio-Zibethinus-hidden" },
-    { selectId: "cha-nee", otherId: "cha-nee-other", hiddenId: "cha-nee-hidden" },
     { selectId: "rambutan", otherId: "rambutan-other", hiddenId: "rambutan-hidden" },
     { selectId: "mangosteen", otherId: "mangosteen-other", hiddenId: "mangosteen-hidden" }
 ];
